3d4e27a66900f8a1922dc5c7489faadf63d86595
GitDarren <darren321@gmail.com>
2017-12-08 21:20:07 -0600

    Everything working except calculations

--- tracker.js
+++ tracker.js
@@ -17,33 +17,58 @@
 
   var name = "";
   var role = "";
-  var startDate;
-  var monthlyRate;
+  var startDate = moment().format("MM-DD-YYYY");
+  var monthlyRate = 0;
   var monthsWorked;
   var totalBilled;
 
-  database.ref().on("value", function(snapshot) {
-
-    $("#em-name").text(name);
-    $("#em-role").text(role);
-  }, function(errorObject) {
-    console.log("The read failed: " + errorObject.code);
-  });
-
+  database.ref().on("child_added", function(snapshot){
+    console.log('here')
+    $('#dostuff').append(
+    `<tr class="active">
+        <td id="table-name">${snapshot.val().inputName}</td>
+        <td id="table-role">${snapshot.val().inputRole}</td>
+        <td id="table-start">${snapshot.val().startDate}</td>
+        <td id="months-worked"></td>
+        <td id="table-rate">${snapshot.val().inputRate}</td>
+        <td id="total-billed"></td>
+    </tr>
+    `)
 
   $("#submit-bid").on("click", function(event) {
     event.preventDefault();
 
-      var inputedName = $("#em-name").val().trim();
-      var bidderPrice = parseInt($("#bidder-price").val().trim());
+      var inputName = $("#em-name").val().trim();
+      var inputRole = $("#em-role").val().trim();
+      var inputRate = parseInt($("#em-rate").val().trim());
+      var startDate = $("#em-start").val().trim();
+
+      // console.log({
+      //   inputName: inputName,
+      //   inputRole: inputRole,
+      //   inputRate: inputRate,
+      //   startDate: startDate,
+      // })
+
+      database.ref().push({
+        inputName: inputName,
+        inputRole: inputRole,
+        inputRate: inputRate,
+        startDate: startDate
+      })
 
 
-      database.ref().set({
 
-      alert("shit is working.");
+     // console.log(snapshot.val());
+     // console.log(snapshot.on("child_added").inputName);
+     // console.log(snapshot.on("child_added").inputRole);
+     // console.log(snapshot.on("child_added").inputRate);
+     // console.log(snapshot.on("child_added").startDate);
 
-    }
-
-
+     // $("#table-name").append(snapshot.val().inputName);
+     // $("#table-role").append(snapshot.val().inputRole);
+     // $("#table-rate").append(snapshot.val().inputRate);
+     // $("#table-start").append(snapshot.val().startDate);
+   });
 
   });
